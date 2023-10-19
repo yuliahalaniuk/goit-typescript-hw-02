d26977c3fa917a721dcb903f5d9d8339f4158f38
Yulia-Kot <yuhalaniuk@ukr.net>
2023-10-19 18:25:09 +0300

    fixed

--- src/generic/1.ts
+++ src/generic/1.ts
@@ -3,13 +3,12 @@
   Доповніть цю функцію, використовуючи generics, щоб вона повертала правильний тип.
 */
 
-function getPromise<T>() {
-  return new Promise<T>((resolve) => {
-    resolve(["Text", 50] as T);
+function getPromise(): Promise<(string | number)[]> {
+  return new Promise((resolve) => {
+    resolve(["Text", 50]);
   });
 }
-
-getPromise<[string, number]>().then((data) => {
+getPromise().then((data) => {
   console.log(data);
 });
 
